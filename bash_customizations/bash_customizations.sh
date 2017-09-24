54b31034c93ac9c5fc57acefca6a149c1de67301
Abhijit Murthy <amurthy@lab126.com>
2017-09-24 00:17:53 -0700

    moved more bash customizations, moved i3 config to .i3/
    Signed-off-by: Abhijit Murthy <amurthy@lab126.com>

--- bash_customizations/bash_customizations.sh
+++ bash_customizations/bash_customizations.sh
@@ -1,4 +1,11 @@
 
+PATH=$PATH:~/bin
+export USE_CCACHE=1
+export LANG=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
+export PATH=/home/local/ANT/amurthy/tools/depot_tools:$BRAZIL_CLI_BIN:$PATH
+
+[ -f ~/.fzf.bash ] && source ~/.fzf.bash
 tmux attach -t perma-session || tmux new-session -s perma-session
 alias m16='make -j16'
 alias vf='vim $(fzf)'
