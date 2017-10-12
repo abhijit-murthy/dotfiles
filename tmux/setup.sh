f26bab033618fd7f1bd85271c939a33c40b05675
Abhijit Murthy <amurthy@lab126.com>
2017-10-11 17:38:36 -0700

    added support for mouse selection and copy
    only works on tmux 2.6+
    
    Signed-off-by: Abhijit Murthy <amurthy@lab126.com>

--- tmux/setup.sh
+++ tmux/setup.sh
@@ -8,6 +8,13 @@ if ! hash tmux; then
 	exit 1;
 fi
 
+VERSION=$(tmux -V | awk -F" " '{print $2}')
+TEST=$(echo $VERSION'>= 2.5 ' | bc -l)
+if [ "$TEST" -eq "0" ]; then
+	echo "tmux version 2.5+ needed";
+	exit 1;
+fi
+
 if [ ! -e ~/.tmux.conf ]; then
 	ln -s "$DIR/.tmux.conf" ~/;
 fi
