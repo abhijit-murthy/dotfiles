dba94c9b9fe0ed6c197ab17ed28d08684d3a0ebe
Abhijit Murthy <amurthy@lab126.com>
2017-11-01 01:56:06 -0700

    Add ack and ack.vim
    Signed-off-by: Abhijit Murthy <amurthy@lab126.com>

--- vim/setup.sh
+++ vim/setup.sh
@@ -8,6 +8,11 @@ if ! hash vim; then
 	exit 1;
 fi
 
+if ! hash ack; then
+	echo "ack missing, installing";
+	curl https://beyondgrep.com/ack-2.18-single-file > ~/bin/ack && chmod 0755 ~/bin/ack;
+fi
+
 if [ ! -e ~/.vimrc ]; then
 	ln -s "$DIR/.vim/" ~/;
 	ln -s "$DIR/.vimrc" ~/;
