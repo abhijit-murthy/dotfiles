ecdc41b52d87fce2cbf62363bcab450bfd7b7c88
Abhijit Murthy <abhijit.murthy@gmail.com>
2019-09-17 22:54:56 -0700

    remove fish, ensure fzf gets installed, set bash to vi input mode
    Signed-off-by: Abhijit Murthy <abhijit.murthy@gmail.com>

--- bash/bash_customizations/bash_customizations.sh
+++ bash/bash_customizations/bash_customizations.sh
@@ -3,4 +3,4 @@ PATH=$PATH:~/bin
 export LANG=en_US.UTF-8
 export LC_ALL=en_US.UTF-8
 [[ -e ~/.custom_script.sh ]] && source ~/.custom_script.sh
-if [ -z "$BASH_EXECUTION_STRING" ]; then exec fish; fi
+set -o vi
--- env/setup.sh
+++ env/setup.sh
@@ -3,6 +3,12 @@
 SCRIPT_PATH=$(realpath $0)
 DIR=$(dirname $SCRIPT_PATH)
 
+if ! hash fzf; then
+	echo "fzf not installed, installing";
+	git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+	~/.fzf/install
+fi
+
 if ! hash playerctl; then
 	echo "install playerctl!";
 	exit 1;
