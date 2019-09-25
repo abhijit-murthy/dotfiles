8edc5158826cdf2f8c5017b116830b3f85511358
Abhijit Murthy <amurthy@lab126.com>
2019-09-25 11:59:06 -0700

    Use fd for fzf instead of find
    Signed-off-by: Abhijit Murthy <amurthy@lab126.com>

--- bash/bash_customizations/bash_customizations.sh
+++ bash/bash_customizations/bash_customizations.sh
@@ -4,3 +4,5 @@ export LANG=en_US.UTF-8
 export LC_ALL=en_US.UTF-8
 [[ -e ~/.custom_script.sh ]] && source ~/.custom_script.sh
 set -o vi
+export FZF_DEFAULT_COMMAND='fd --type f'
+export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
