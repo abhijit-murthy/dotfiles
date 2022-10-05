eeab2166dcc1f1312fe0c6fd81f3c6955e1725e7
Abhijit Murthy <abhijit@hu.ma.ne>
2022-10-05 01:19:40 -0700

    Nixify tmux config
    Home Manager can generate tmux configs. Use that and add some new things
    to the tmux configurations:
    - Use tmux plugins
    - Install gruvbox themes (no more hardcoded themeing!)
    - Install tmux sensible plugin
    - Install tmux yank plugin
    
    Signed-off-by: Abhijit Murthy <abhijit@hu.ma.ne>

--- home.nix
+++ home.nix
@@ -52,7 +52,37 @@
     "$HOME/.cargo/bin"
   ];
 
+  programs.tmux = {
+    enable = true;
+    keyMode = "vi";
+    prefix = "C-a";
+    terminal = "screen-256color";
+    shell = "~/.nix-profile/bin/zsh";
+    plugins = with pkgs; [
+      tmuxPlugins.sensible
+      tmuxPlugins.yank
+      {
+        plugin = tmuxPlugins.gruvbox;
+        extraConfig = "set -g @tmux-gruvbox 'dark'";
+      }
+    ];
+    extraConfig = ''
+        bind | split-window -h
+        bind - split-window -v
+        unbind '"'
+        unbind %
+        set -g mouse on
+        # Sensible overwrites this to
+        # the user default shell. But
+        # if the default isn't zsh you'll
+        # drop into the user default (or
+        # the system zsh). Clear this so
+        # tmux starts the nix zsh by default.
+        set -g default-command ""
+    '';
+  };
+
+
   home.file.".config/nvim".source = ./vim;
-  home.file.".tmux.conf".source = ./tmux/.tmux.conf;
   home.file.".gitconfig.custom".source = ./git/.gitconfig.custom;
 }
