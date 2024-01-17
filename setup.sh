342f64b217f4a7495af0c4fbb044692d6a49e00c
Abhijit Murthy <abhijit.murthy@gmail.com>
2024-01-16 21:01:29 -0800

    add alacritty config
    Signed-off-by: Abhijit Murthy <abhijit.murthy@gmail.com>

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ if ! command -v stow 2>&1 1>/dev/null; then
     echo "install stow!";
 fi
 
-stow scripts tmux vim zsh
+stow scripts tmux vim zsh alacritty
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
