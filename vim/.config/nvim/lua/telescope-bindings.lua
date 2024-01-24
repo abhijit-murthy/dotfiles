1666daf09fb0d62c303403f1e0e41be3baf1dde4
Abhijit Murthy <abhijit.murthy@gmail.com>
2024-01-23 20:23:13 -0800

    change telescope find files binding to avoid keymap resolution timeout
    Signed-off-by: Abhijit Murthy <abhijit.murthy@gmail.com>

--- vim/.config/nvim/lua/telescope-bindings.lua
+++ vim/.config/nvim/lua/telescope-bindings.lua
@@ -6,16 +6,5 @@ vim.keymap.set('n', 'gS', builtin.lsp_workspace_symbols, {})
 vim.keymap.set('n', 'ge', builtin.diagnostics, {})
 vim.keymap.set('n', 'gs', builtin.lsp_document_symbols, {})
 
-function find_files_binding()
-  builtin.find_files {
-    hidden = true
-  }
-end
-vim.keymap.set('n', '<leader>f', find_files_binding, {})
-
-function live_grep_binding()
-  builtin.live_grep {
-    hidden = true
-  }
-end
-vim.keymap.set('n', '<leader>g', live_grep_binding, {})
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>g', builtin.live_grep, {})
